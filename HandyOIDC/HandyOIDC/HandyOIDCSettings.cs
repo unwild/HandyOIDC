769cc4dd4570ae08bbfcbb08e3a6525c1dd6147b
DESKTOP-31BT92A\Hugo <hugorobert41@gmail.com>
2019-04-03 12:46:04 +0200

    Added example webApp

---
+++ HandyOIDC/HandyOIDC/HandyOIDCSettings.cs
@@ -0,0 +1,19 @@
+﻿namespace HandyOIDC
+{
+    public class HandyOIDCSettings
+    {
+        public string AuthorizationURL { get; set; }
+
+        public string TokenURL { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string CallbackURL { get; set; }
+
+        public string Scope { get; set; } = "openid";
+
+        public string AuthFailURL { get; set; } = null;
+    }
+}
---
+++ HandyOIDC/WebApplication/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using HandyOIDC;
+
+namespace WebApplication
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            HandyOIDC.HandyOIDC.Init(new HandyOIDCSettings()
+            {
+                AuthorizationURL = "https://myprovider.xyz/authorize",
+                TokenURL = "https://myprovider.xyz/token",
+                ClientId = "myclientid",
+                ClientSecret = "myclientsecret",
+                CallbackURL = "http://localhost:63835/callback"
+            });
+
+        }
+
+
+        protected void Application_AcquireRequestState(Object sender, EventArgs e)
+        {
+
+            HandyOIDC.HandyOIDC.HandleLogin(HttpContext.Current);
+
+        }
+
+    }
+}
